10d5e2f6e31ba416184632cc17b01f6211e3cd14
MDMOURSHAD <77190784+MDMOURSHAD@users.noreply.github.com>
2021-06-22 16:18:19 +0600

    Add files via upload

---
+++ assests/js/main.js
@@ -0,0 +1,13 @@
+
+
+// MOBILE MENU //
+
+const menuIcon = document.querySelector('.hamburger-menu');
+const navlist = document.querySelector('.nav-list');
+
+menuIcon.addEventListener("click", () => {
+
+
+	menuIcon.classList.toggle('change');
+	navlist.classList.toggle('slide-menu');
+})
