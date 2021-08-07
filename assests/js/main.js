83323ace4181ee046fbb90eb7a3577c3457d16fe
MDMOURSHAD <77190784+MDMOURSHAD@users.noreply.github.com>
2021-08-07 09:32:50 +0600

    Add files via upload

--- assests/js/main.js
+++ assests/js/main.js
@@ -7,7 +7,14 @@ const navlist = document.querySelector('.nav-list');
 
 menuIcon.addEventListener("click", () => {
 
-	menuIcon.classList.toggle('change');
-	navlist.classList.toggle('slide-menu');
-	
+  menuIcon.classList.toggle('change');
+  navlist.classList.toggle('slide-menu');
+  
 });
+
+function myFunction(){
+
+  menuIcon.classList.remove('change');
+  navlist.classList.remove('slide-menu');
+  
+}
